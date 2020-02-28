a234140f369cf7b3e2da8d9bab444c774a3492b9
nxan <nektarios.xanthakis@eurodyn.com>
2020-02-28 15:16:16 +0200

    Add ingredients to recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,26 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'Simply test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Another Test Recipe', 'Simply test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe(
+      'Burger',
+      'Just a burger',
+      'https://cdn.pixabay.com/photo/2017/12/29/12/56/burger-3047550_960_720.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+        new Ingredient('Tomatoes', 1)
+      ]),
+    new Recipe(
+      'Schnitzel',
+      'This is a schnitzel',
+      'https://cdn.pixabay.com/photo/2019/03/25/20/20/schnitzel-4081269_960_720.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ])
   ];
   recipeSelected = new EventEmitter<Recipe>();
   getRecipes(){
