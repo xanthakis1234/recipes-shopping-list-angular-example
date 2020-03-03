e6c8b80196fc60500fbf2962085b54eb9b3ef8bf
nxan <nektarios.xanthakis@eurodyn.com>
2020-03-03 18:36:35 +0200

    Add Sign up shell

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  onSwitchMode(){
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    this.isLoading = true;
+    if (this.isLoginMode){
+      //something
+    } else {
+      this.authService.signup(email, password).subscribe(
+        response => {
+          console.log(response);
+          this.isLoading = false;
+        },
+        error => {
+          console.log(error);
+          this.error = 'An error occurred';
+          this.isLoading = false;
+        }
+      );
+    }
+    form.reset();
+  }
 }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  signup(email: string, password: string){
+    return this.http
+      .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB3nyeImJnVJE3_VwyPx4-YQk3ZEGNEiUI',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true
+        });
+  }
+}
