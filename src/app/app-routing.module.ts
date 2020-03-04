a5ee29cc6f817bc23d2fa95e090486f12b4ca6e0
nxan <nektarios.xanthakis@eurodyn.com>
2020-03-04 17:58:16 +0200

    Refactor into Feature, core and shared modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,9 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {RecipesComponent} from "./recipes/recipes.component";
-import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
-import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
-import {RecipeDetailsComponent} from "./recipes/recipe-details/recipe-details.component";
-import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
-import {RecipesResolverService} from "./recipes/recipes-resolver.service";
-import {AuthComponent} from "./auth/auth.component";
-import {AuthGuard} from "./auth/auth.guard";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-    canActivate: [AuthGuard],
-    children: [
-      {path: '', component: RecipeStartComponent },
-      {path: 'new', component: RecipeEditComponent },
-      {path: ':id', component: RecipeDetailsComponent, resolve: [RecipesResolverService] },
-      {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] }
-    ]},
-  { path: 'shopping-list', component: ShoppingListComponent },
-  { path: 'auth', component: AuthComponent }
+  { path: '', redirectTo: '/recipes', pathMatch: 'full'}
 ];
 
 @NgModule({
