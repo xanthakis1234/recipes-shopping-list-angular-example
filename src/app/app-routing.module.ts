51da9eb4d623257bd2d88ac63b2343a183e5acb0
nxan <nektarios.xanthakis@eurodyn.com>
2020-03-04 16:01:04 +0200

    Add Authentication guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,16 @@ import {RecipeDetailsComponent} from "./recipes/recipe-details/recipe-details.co
 import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
 import {RecipesResolverService} from "./recipes/recipes-resolver.service";
 import {AuthComponent} from "./auth/auth.component";
+import {AuthGuard} from "./auth/auth.guard";
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  { path: 'recipes', component: RecipesComponent, children: [
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    canActivate: [AuthGuard],
+    children: [
       {path: '', component: RecipeStartComponent },
       {path: 'new', component: RecipeEditComponent },
       {path: ':id', component: RecipeDetailsComponent, resolve: [RecipesResolverService] },
